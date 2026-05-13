90f3e72d50046666a6f4ad36cd432a1acf16b1dd
oky <oky.project.mail@gmail.com>
2026-05-13 23:59:20 +0700

    add cursor-pointer for approve and reject button

--- src/app/request/[id]/page.tsx
+++ src/app/request/[id]/page.tsx
@@ -116,7 +116,7 @@ export default async function RequestDetailPage({
           >
             <button
               type="submit"
-              className="bg-green-600 text-white px-4 py-2 rounded"
+              className="bg-green-600 text-white px-4 py-2 rounded cursor-pointer"
             >
               Approve
             </button>
@@ -131,7 +131,7 @@ export default async function RequestDetailPage({
           >
             <button
               type="submit"
-              className="bg-red-600 text-white px-4 py-2 rounded"
+              className="bg-red-600 text-white px-4 py-2 rounded cursor-pointer"
             >
               Reject
             </button>
